7229679c3d24c25302c61713101088d8388b6871
ando.mihaly <ando.mihaly@gmail.com>
2023-01-10 16:13:46 +0100

    refactore: remove intProject

--- src/main/java/eu/misi/starter/Starter.java
+++ src/main/java/eu/misi/starter/Starter.java
@@ -1,16 +1,8 @@
 package eu.misi.starter;
 
-import eu.misi.initproject.InitProject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Starter {
 
-    private static final Logger logger = LoggerFactory.getLogger("LogToFile");
-
-
     public static void main(String[] args) {
-        InitProject initProject = new InitProject();
-        initProject.basicString();
+
     }
 }
